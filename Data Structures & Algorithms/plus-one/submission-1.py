978f911fa3216b320f241b85bfa0453038392c03
dannyhua12 <141593321+dannyhua12@users.noreply.github.com>
2026-07-28 00:50:40 -0400

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        res = 0
+        for digit in digits:
+            res*=10
+            res+=digit
+        
+        res+=1
+        n = len(str(res))
+        sol = [0]*n
+
+        for i in range(n):
+            sol[i] = int(str(res)[i])
+        return sol
+
